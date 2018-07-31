3528fceb52666d2c6549eedcc89a128af43e7fbb
Jay Kinderknecht <jaykinderknecht@Jay-Kinderknecht-MacBook-Pro.local>
2018-07-31 18:43:21 -0500

    updates

--- public/gallery.js
+++ public/gallery.js
@@ -3,7 +3,8 @@ var images = [
   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAp3jykUwsdXvF6HlTTl8d-eUo7xSLVj8_xmOQo2d8u0DiZEkZww',
   'https://i.pinimg.com/originals/e2/80/dd/e280dd8ac72b863179244397b69da5d8.jpg',
   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkWuKKqqeDR-OkyIv8A0GJp0UazcfvC3r4gpM4yn_N0a4g9zgUMA',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4GovHPkZa8oZJwzsZybqlU86Fu6PVyxrUPZEtdI7SCN_f2fpy'
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4GovHPkZa8oZJwzsZybqlU86Fu6PVyxrUPZEtdI7SCN_f2fpy',
+  'http://m.memegen.com/4ntydq.jpg'
 ];
 
 var galleryBox = document.querySelector('.gallery');
